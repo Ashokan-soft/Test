cba6c0bb0e0fd588cf652b3e9ff6774c2b576d5d
Ashok <ashokansoft@gmail.com>
2023-02-08 16:17:37 +0530

    all component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AccountComponent } from './account/account.component';
+import { PasswordComponent } from './password/password.component';
+import { SecurityComponent } from './security/security.component';
+import { ApplicationComponent } from './application/application.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AccountComponent,
+    PasswordComponent,
+    SecurityComponent,
+    ApplicationComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule,
