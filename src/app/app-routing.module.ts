eab855cc299f73ab24e95d6850548c3abbe9d2d1
Ashok <ashokansoft@gmail.com>
2023-02-08 22:18:29 +0530

    ident push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { ApplicationComponent } from './application/application.component';
+import { MainComponent } from './main/main.component';
+import { NotificationComponent } from './notification/notification.component';
+import { PasswordComponent } from './password/password.component';
+import { SecurityComponent } from './security/security.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Component',component:MainComponent},
+  {path:'component',component:AccountComponent},
+  {path:'component',component:ApplicationComponent},
+  {path:'component',component:NotificationComponent},
+  {path:'component',component:PasswordComponent},
+  {path:'component',component:SecurityComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
