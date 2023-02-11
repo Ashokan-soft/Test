119c2dd5d0ba8270d4db5cf3d3dc304a569992e8
Ashok <ashokansoft@gmail.com>
2023-02-11 15:59:21 +0530

    checkup

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,14 +10,9 @@ export class MainComponent {
 
 
   ChangeButtoncolor(value: any) {
-<<<<<<< HEAD
     
     this.show = value
     // alert(this.show)
-=======
-    this.show = value
-    //alert(this.show)
->>>>>>> c6c3dca04611f029ca9ef2880f90dc1ed9e0f1a3
   }
 
 
