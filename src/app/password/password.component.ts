c1446b8f7fbe809ade396c47dd6519902e03db57
Ashok <ashokansoft@gmail.com>
2023-02-11 16:36:10 +0530

    Password Field is updated

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -7,5 +7,62 @@ import { Component } from '@angular/core';
 })
 export class PasswordComponent {
 
-  oldPassword:any;
+  OldPassword: any;
+  NewPassword: any;
+  ConfrimPassword: any;
+
+  // smalls vsariables
+  OldPasswordSmall: any;
+  NewPasswordSmall: any;
+  ConfrimPasswordSmall: any;
+  // success message
+  success: any
+
+  update() {
+
+    if (this.OldPassword && this.NewPassword && this.ConfrimPassword != null) {
+      this.OldPasswordSmall = ""
+      this.NewPasswordSmall = ""
+      this.ConfrimPasswordSmall = ""
+
+      if (this.NewPassword != this.ConfrimPassword) {
+
+        this.ConfrimPasswordSmall = "Password not Match"
+
+      }
+      else {
+
+        console.log("Your old password : " + this.OldPassword)
+        console.log("Your New password : " + this.NewPassword)
+        console.log("Your Confrim password : " + this.ConfrimPassword)
+
+        this.success = "successfully Saved"
+      }
+
+    }
+    else {
+
+      if (this.OldPassword == null) {
+        this.OldPasswordSmall = "Field is Empty"
+      }
+      else {
+        this.OldPasswordSmall = ""
+      }
+      if (this.NewPassword == null) {
+        this.NewPasswordSmall = "Field is Empty"
+      }
+      else {
+        this.NewPasswordSmall = ""
+      }
+      if (this.ConfrimPassword == null) {
+        this.ConfrimPasswordSmall = "Field is Empty"
+      }
+      else {
+        this.ConfrimPasswordSmall = ""
+      }
+
+    }
+  }
+
+
 }
